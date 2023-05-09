ed381ae44fd0ec0ad91d4599639451c1cc7578f1
Javier <d2j5@hotmail.com>
2023-05-08 21:48:00 -0400

    final work

--- src/Casting.java
+++ src/Casting.java
@@ -1,7 +1,7 @@
 public class Casting {
     public static void main(String[] args) {
-       /*Create two variables. One variable should have a data type of double and the other one
-        should have a data type of int. Set the double variable equal to 112.35 and set the in
+       /*Create two variables. One variable should have a data type of double and the other
+       one should have a data type of int. Set the double variable equal to 112.35 and set the in
         value to the double variable. You must explicitly cast your double into an in for the
         second variable. Please print them both to the console to match the below output. */
         double data = 112.35;
